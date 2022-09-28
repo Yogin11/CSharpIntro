4b986752b59e7ea6b844b7cef39a14fdcec7f782
MaxYogin <yogin.eleven@gmail.com>
2022-09-28 21:22:34 +0300

    Random in OO3SUM file

--- Example001UserHello/Program.cs
+++ Example001UserHello/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.Write("Enter YOUR NAME ");
-string username=Console.ReadLine(); 
+string username = System.Console.ReadLine(); 
 
 Console.Write("Привет, ");
 Console.Write(username);
