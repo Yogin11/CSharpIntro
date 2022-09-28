20d363dcb7dc666f200651f03adf2e415314dc3c
MaxYogin <yogin.eleven@gmail.com>
2022-09-28 20:30:13 +0300

    Added seconnd folder - User Hello

---
+++ Example001UserHello/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter YOUR NAME ");
+string username=Console.ReadLine(); 
+
+Console.Write("Привет, ");
+Console.Write(username);
+
