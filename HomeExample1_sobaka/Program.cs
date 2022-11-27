7b99b9e2e30d02dfd07de0e7e4949c738457cdb2
MaxYogin <yogin.eleven@gmail.com>
2022-11-27 14:51:16 +0300

    added HomeExample_sobaka

---
+++ HomeExample1_sobaka/Program.cs
@@ -0,0 +1,20 @@
+﻿int V1 = 1, V2 = 2, Vs = 5;
+int distance = 10000, count = 0, friend = 2;
+int time;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (V1 + Vs);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (V2 + Vs);
+        friend = 1;
+    }
+    distance = distance - time * (V1 + V2);
+    count += 1;
+}
+Console.Clear();
+Console.WriteLine("Собака пробежит " + count + " раз");
